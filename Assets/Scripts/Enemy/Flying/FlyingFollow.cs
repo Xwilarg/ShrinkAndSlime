f9e1ef1d0fe8a82044658836457ca0e88cc03735
Xwilarg <xwilarg@protonmail.com>
2024-10-05 03:42:11 +0200

    Make insect look at player

--- Assets/Scripts/Enemy/Flying/FlyingFollow.cs
+++ Assets/Scripts/Enemy/Flying/FlyingFollow.cs
@@ -10,6 +10,10 @@ namespace LudumDare56.Enemy.Flying
             var revDir = PlayerController.Instance.transform.position + ((transform.position - PlayerController.Instance.transform.position).normalized * 5f) - (Vector3.one * 5f);
             revDir.y = 5f;
 
+            var prev = transform.eulerAngles;
+            transform.LookAt(PlayerController.Instance.transform);
+            transform.eulerAngles = new(prev.x, transform.eulerAngles.y, prev.z);
+
             MoveTowards(revDir, 5f);
         }
     }
