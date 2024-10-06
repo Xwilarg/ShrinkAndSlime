6c040ed25539bc1021e53b482323da0ec34fc546
Xwilarg <xwilarg@protonmail.com>
2024-10-06 17:42:05 +0200

    Add BGM

---
+++ Assets/Scripts/Manager/BGMManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace LudumDare56.Manager
+{
+    public class BGMManager : MonoBehaviour
+    {
+        [SerializeField]
+        private AudioSource _bgm;
+
+        private void Update()
+        {
+            if (!_bgm.isPlaying)
+            {
+                _bgm.time = 12.97f;
+                _bgm.Play();
+            }
+        }
+    }
+}
